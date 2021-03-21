6896262163f8bb1df33c8cfe1d315231a60c10b0
Garrett G <77699110+GGehle@users.noreply.github.com>
2021-03-21 01:39:10 -0500

    round 2
    1:39

--- insert.php
+++ insert.php
@@ -11,7 +11,7 @@ if(isset($_POST['submit']))
      $lease_end_date  = $_POST['lease_end_date '];
      $renew_lease  = $_POST['renew_lease '];
      $pay_by_date  = $_POST['pay_by_date '];
-     $sql = "INSERT INTO lease_agreements (rental_name , property_address , deposit_amount , tenant_reference , notice_period , lease_start_date , lease_end_date , renew_lease , pay_by_date)
+     $sql = "INSERT INTO lease_agreements (rental_name,property_address,deposit_amount,tenant_reference,notice_period,lease_start_date,lease_end_date,renew_lease,pay_by_date)
      VALUES ('$rental_name','$property_address', '$deposit_amount','$tenant_reference','$notice_period','$lease_start_date','$lease_end_date','$renew_lease','$pay_by_date')";
      if (mysqli_query($conn, $sql)) {
         echo "New rental is pending creation!";
