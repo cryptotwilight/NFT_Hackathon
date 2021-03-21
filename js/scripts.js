159631b0f17ce95c4806b8a9d95e721ab3858a10
Garrett G <77699110+GGehle@users.noreply.github.com>
2021-03-21 00:06:44 -0500

    test test test
    is this thing on?

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+if (access_hash == "1234") {
+    //  block of code to be executed if the condition is true
+    window.location = 'tenant_info.html';
+  } 
+
+
 function initializeJS() {
     // Get the modal
 var modal = document.getElementById("myModal");
